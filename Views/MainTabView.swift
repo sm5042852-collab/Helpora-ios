0c3dda3949899eb1779155ea5afeb2efd38f3ba8
sm5042852-collab <sm5042852@gmail.com>
2026-09-14 08:34:04 +0200

    feat: expand Views/MainTabView.swift

--- Views/MainTabView.swift
+++ Views/MainTabView.swift
@@ -6,8 +6,14 @@ struct MainTabView: View {
             HomeView()
                 .tabItem { Label("Home", systemImage: "house.fill") }
 
+            DiscoverView()
+                .tabItem { Label("Discover", systemImage: "safari.fill") }
+
             HelpView()
                 .tabItem { Label("Help", systemImage: "heart.fill") }
+
+            InboxView()
+                .tabItem { Label("Inbox", systemImage: "bubble.left.and.bubble.right.fill") }
 
             ProfileView()
                 .tabItem { Label("Profile", systemImage: "person.fill") }
