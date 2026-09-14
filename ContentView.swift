c40f269f767339311d4b0a90e92ae8bc04d9216f
sm5042852-collab <sm5042852@gmail.com>
2026-09-14 08:21:23 +0200

    feat: add ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView().tabItem { Label("Home", systemImage: "house.fill") }
+            HelpView().tabItem { Label("Help", systemImage: "heart.fill") }
+            ProfileView().tabItem { Label("Profile", systemImage: "person.fill") }
+        }
+        .tint(.purple)
+    }
+}
