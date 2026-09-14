c7beec218cd381f3db000a39a103ddaca61e3800
sm5042852-collab <sm5042852@gmail.com>
2026-09-14 08:37:38 +0200

    feat: expand HelporaApp.swift

--- HelporaApp.swift
+++ HelporaApp.swift
@@ -3,11 +3,15 @@ import SwiftUI
 @main
 struct HelporaApp: App {
     @StateObject private var helpStore = HelpRequestStore()
+    @StateObject private var profileStore = ProfileStore()
+    @StateObject private var notificationStore = NotificationStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(helpStore)
+                .environmentObject(profileStore)
+                .environmentObject(notificationStore)
         }
     }
 }
